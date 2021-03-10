15f1d6d72756de80e936caa8616f4680359dbd4d
Emmanuel Koumandakis <manykoum@msn.com>
2021-03-10 11:11:30 +0200

    some of basler.mat specs
    they need to change either way but moving development to a different computer

--- RadioStudyFromConfig.m
+++ RadioStudyFromConfig.m
@@ -1,9 +1,8 @@
 % assume there is a sensor and a lens .mat file in configs
 
 %% Initialization
-% Make the calculations from 1 to 100km
-dist_arr = logspace(-1, 9, 200); % Relative distance in m [1m, 10e+5m]
-times = [3.6506e-05, 2*3.6506e-05, 1e-04, 1e-03, 1e-02, 1e-01]; % Exposure times in s
+% Make the calculations from 1 m to 1000 km
+dist_arr = logspace(-1, 9, 200); % Relative distance in m [1m, 10e+9m]
 times = [3.6506e-05, 2*3.6506e-05, 1e-04, 1e-01, 0.2, 1e-0]; % Exposure times in s
 l = length(dist_arr);
 t_l = length(times);
@@ -14,3 +13,4 @@ target_detection_SNR = 2.0;
 wpp = zeros(1, l); % Incident power in watts per pixel
 saturation = zeros(t_l, l); % Array w/ saturation intensity  
 p = zeros(1, t_l);
+
